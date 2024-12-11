3ccea088c30d5a69d6d88f2a87825d7bd7b17b56
Aris Goudouras <aris.gk@gmail.com>
2024-12-11 20:48:33 +0200

    feat: parse block types

--- src/Automerge.hs
+++ src/Automerge.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE InstanceSigs #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module Automerge where
@@ -10,28 +11,6 @@ import Data.Aeson
   )
 import Data.Aeson.Types (Parser)
 import qualified Data.Text as T
-
-data BlockType
-  = Paragraph
-  | Heading
-  | CodeBlock
-  | BlockQuote
-  | OrderedListItem
-  | UnorderedListItem
-  | ImageBlock
-  deriving (Show)
-
-instance FromJSON BlockType where
-  parseJSON = withText "BlockType" $ \t ->
-    case t of
-      "paragraph" -> pure Paragraph
-      "heading" -> pure Heading
-      "code-block" -> pure CodeBlock
-      "blockquote" -> pure BlockQuote
-      "ordered-list-item" -> pure OrderedListItem
-      "unordered-list-item" -> pure UnorderedListItem
-      "image" -> pure ImageBlock
-      _ -> fail "Invalid value for BlockType"
 
 data Mark
   = Strong
@@ -47,15 +26,45 @@ instance FromJSON Mark where
       "link" -> pure Link
       _ -> fail "Invalid value for Mark"
 
+-- TODO: Constrain to levels 1-6
+newtype HeadingLevel = HeadingLevel Int deriving (Show)
+
+newtype Heading = Heading HeadingLevel deriving (Show)
+
+data BlockSpan
+  = ParagraphSpan
+  | HeadingSpan Heading
+  | CodeBlockSpan
+  | BlockQuoteSpan
+  | OrderedListItemSpan
+  | UnorderedListItemSpan
+  | ImageBlockSpan
+  deriving (Show)
+
+data TextSpan = TextSpan {value :: T.Text, marks :: [Mark]} deriving (Show)
+
 data AutomergeSpan
-  = BlockSpan {blockType :: BlockType}
-  | TextSpan {value :: T.Text, marks :: [Mark]}
+  = Block BlockSpan
+  | Inline TextSpan
   deriving (Show)
 
 instance FromJSON AutomergeSpan where
   parseJSON = withObject "AutomergeSpan" $ \v -> do
     elementType <- (v .: "type" :: Parser String)
     case elementType of
-      "block" -> BlockSpan <$> (v .: "value" >>= (.: "type"))
-      "text" -> TextSpan <$> v .: "value" <*> v .: "marks"
+      "block" -> do
+        blockData <- v .: "value"
+        blockType <- (blockData .: "type" :: Parser String)
+        case blockType of
+          "paragraph" -> pure $ Block ParagraphSpan
+          "heading" -> do
+            level <- (blockData .: "level" :: Parser Int)
+            pure $ Block $ HeadingSpan $ Heading $ HeadingLevel level
+          "code-block" -> pure $ Block CodeBlockSpan
+          "blockquote" -> pure $ Block BlockQuoteSpan
+          "ordered-list-item" -> pure $ Block OrderedListItemSpan
+          "unordered-list-item" -> pure $ Block UnorderedListItemSpan
+          "image" -> pure $ Block ImageBlockSpan
+          _ -> fail "Invalid block type"
+      "text" -> Inline <$> (TextSpan <$> v .: "value" <*> v .: "marks")
       _ -> fail "Unknown span type"
