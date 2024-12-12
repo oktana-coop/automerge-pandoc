1d4a4a4de0ad728776b7dd5d44c2fb83b40af9b3
Aris Goudouras <aris.gk@gmail.com>
2024-12-12 12:04:05 +0200

    feat: parse marks as a key map, not a list

--- src/Automerge.hs
+++ src/Automerge.hs
@@ -8,13 +8,14 @@ import Data.Aeson
     eitherDecode,
     withObject,
     withScientific,
-    withText,
     (.:),
     (.:?),
   )
-import Data.Aeson.Types (Parser)
+import qualified Data.Aeson.Key as K
+import qualified Data.Aeson.KeyMap as KM
+import Data.Aeson.Types (Parser, Value (Bool), (.!=))
 import qualified Data.ByteString.Lazy as BL
-import Data.Maybe (fromMaybe)
+import Data.Maybe (mapMaybe)
 import qualified Data.Text as T
 
 data Mark
@@ -22,14 +23,6 @@ data Mark
   | Emphasis
   | Link
   deriving (Show)
-
-instance FromJSON Mark where
-  parseJSON = withText "Mark" $ \t ->
-    case t of
-      "strong" -> pure Strong
-      "em" -> pure Emphasis
-      "link" -> pure Link
-      _ -> fail "Invalid value for Mark"
 
 -- TODO: Constrain to levels 1-6
 newtype HeadingLevel = HeadingLevel Int deriving (Show)
@@ -88,8 +81,19 @@ parseBlock v = do
 parseInline :: Object -> Parser AutomergeSpan
 parseInline v = do
   parsedValue <- v .: "value"
-  parsedMarks <- fromMaybe [] <$> v .:? "marks"
+  marksKeyMap <- v .:? "marks" .!= KM.empty
+  let parsedMarks = parseMarks marksKeyMap
   pure $ Inline $ TextSpan parsedValue parsedMarks
+
+parseMarks :: KM.KeyMap Value -> [Mark]
+parseMarks = mapMaybe parseMark . KM.toList
+  where
+    parseMark (k, Bool True) = case K.toText k of
+      "strong" -> Just Strong
+      "em" -> Just Emphasis
+      "link" -> Just Link
+      _ -> Nothing
+    parseMark _ = Nothing
 
 parseAutomergeSpans :: BL.ByteString -> Either String [AutomergeSpan]
 parseAutomergeSpans = eitherDecode
