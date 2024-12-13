d85db125dcf08a580f567980436aeb2198f5eaab
Aris Goudouras <aris.gk@gmail.com>
2024-12-13 16:10:29 +0200

    refactor: extract spans folding in a separate function

--- src/PandocReader.hs
+++ src/PandocReader.hs
@@ -7,16 +7,19 @@ import Text.Pandoc.Builder (Blocks, Many (..))
 import Text.Pandoc.Class
 import Text.Pandoc.Definition
 
-convertAutomergeSpans :: (PandocMonad m) => [AutomergeSpan] -> m Pandoc
-convertAutomergeSpans spans = pure . Pandoc nullMeta . toList $ foldl' convertAutomergeSpan (Many Seq.Empty) spans
+toPandoc :: (PandocMonad m) => [AutomergeSpan] -> m Pandoc
+toPandoc spans = pure . Pandoc nullMeta . toList $ convertAutomergeSpans spans
+
+convertAutomergeSpans :: [AutomergeSpan] -> Blocks
+convertAutomergeSpans = foldl' convertAutomergeSpan (Many Seq.Empty)
 
 convertAutomergeSpan :: Blocks -> AutomergeSpan -> Blocks
 convertAutomergeSpan acc (BlockSpan blockSpan) = acc <> convertBlockSpan blockSpan
-convertAutomergeSpan acc (TextSpan textSpan) = Many Seq.Empty -- Empty sequence of blocks for TextSpan
+convertAutomergeSpan acc (TextSpan textSpan) = Many Seq.Empty
 
 convertBlockSpan :: BlockMarker -> Blocks
 convertBlockSpan blockSpan = case blockSpan of
-  ParagraphMarker -> Many . singleton $ Para [] -- Wrap Para [] in a singleton sequence
+  ParagraphMarker -> Many . singleton $ Para []
   HeadingMarker (Heading (HeadingLevel level)) -> Many . singleton $ Header level nullAttr []
   CodeBlockMarker -> undefined -- To be implemented
   _ -> undefined -- To be implemented
