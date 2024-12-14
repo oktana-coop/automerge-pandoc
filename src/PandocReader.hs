fa836b56702b30d70000a6f516a6b4e705a0049b
Aris Goudouras <aris.gk@gmail.com>
2024-12-14 19:21:30 +0200

    feat: convert text spans to pandoc blocks

--- src/PandocReader.hs
+++ src/PandocReader.hs
@@ -1,10 +1,10 @@
 module PandocReader where
 
-import Automerge (AutomergeSpan (..), BlockMarker (..), Heading (..), HeadingLevel (..), TextSpan (..))
+import Automerge (AutomergeSpan (..), BlockMarker (..), Heading (..), HeadingLevel (..), Mark (..), TextSpan (..))
 import Data.Foldable (foldl') -- Import foldl' from Data.Foldable
 import Data.Sequence as Seq (Seq (Empty))
 import qualified Data.Text as T
-import Text.Pandoc.Builder (Blocks, Many (..), blockQuote, codeBlock, doc, header, para, str)
+import Text.Pandoc.Builder (Blocks, Inlines, Many (..), blockQuote, codeBlock, codeBlockWith, doc, emph, header, headerWith, para, str, strong)
 import Text.Pandoc.Class
 import Text.Pandoc.Definition
 import Utils (lastValue)
@@ -17,7 +17,7 @@ convertAutomergeSpans = foldl' convertAutomergeSpan (Many Seq.Empty)
 
 convertAutomergeSpan :: Blocks -> AutomergeSpan -> Blocks
 convertAutomergeSpan acc (BlockSpan blockSpan) = acc <> convertBlockSpan blockSpan
-convertAutomergeSpan acc (TextSpan textSpan) = acc <> convertTextSpan (lastValue acc) textSpan
+convertAutomergeSpan acc (TextSpan textSpan) = acc <> convertAndWrapTextSpan (lastValue acc) textSpan
 
 convertBlockSpan :: BlockMarker -> Blocks
 convertBlockSpan blockSpan = case blockSpan of
@@ -27,6 +27,26 @@ convertBlockSpan blockSpan = case blockSpan of
   BlockQuoteMarker -> blockQuote $ Many Seq.Empty
   _ -> undefined -- more blocks to be implemented
 
-convertTextSpan :: Maybe Block -> TextSpan -> Blocks
-convertTextSpan Nothing textSpan = para $ str $ value textSpan -- TODO: handle marks
-convertTextSpan (Just block) textSpan = undefined
+convertAndWrapTextSpan :: Maybe Block -> TextSpan -> Blocks
+convertAndWrapTextSpan Nothing textSpan = para $ convertTextSpan textSpan
+convertAndWrapTextSpan (Just block) textSpan = case block of
+  Para _ -> para $ convertTextSpan textSpan
+  Header level attr _ -> headerWith attr level $ convertTextSpan textSpan
+  CodeBlock attr _ -> codeBlockWith attr $ value textSpan
+  BlockQuote _ -> blockQuote $ para $ convertTextSpan textSpan
+  _ -> undefined
+
+convertTextSpan :: TextSpan -> Inlines
+convertTextSpan = convertMarksToInlines <*> convertTextToInlines
+
+convertTextToInlines :: TextSpan -> Inlines
+convertTextToInlines = str . value
+
+convertMarksToInlines :: TextSpan -> Inlines -> Inlines
+convertMarksToInlines textSpan inlines = foldl' (flip markToInlines) inlines $ marks textSpan
+
+markToInlines :: Mark -> Inlines -> Inlines
+markToInlines mark = case mark of
+  Automerge.Strong -> strong
+  Automerge.Emphasis -> emph
+  _ -> undefined -- TODO: Handle link
