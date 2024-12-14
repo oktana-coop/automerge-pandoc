03a4ea540509786574ee5eb86d45de9ebadacdfe
Aris Goudouras <aris.gk@gmail.com>
2024-12-14 20:33:22 +0200

    feat: leverage pandoc in main to do the conversion

--- app/Main.hs
+++ app/Main.hs
@@ -2,17 +2,26 @@ module Main (main) where
 
 import Automerge (parseAutomergeSpans)
 import qualified Data.ByteString.Lazy.Char8 as BL
+import qualified Data.Text.IO as TIO
+import PandocReader (toPandoc)
 import System.Environment (getArgs)
+import Text.Pandoc (def)
+import Text.Pandoc.Class (runIO)
+import Text.Pandoc.Error (handleError)
+import Text.Pandoc.Writers (writeMarkdown)
 
 main :: IO ()
 main = do
   args <- getArgs
   case args of
-    [input] -> processJSON $ BL.pack input
+    [input] -> do
+      let automergeSpans = parseAutomergeSpans $ BL.pack input
+      case automergeSpans of
+        Left err -> putStrLn $ "Error: " ++ err
+        Right spans -> do
+          result <- runIO $ do
+            doc <- toPandoc spans
+            writeMarkdown def doc
+          rst <- handleError result
+          TIO.putStrLn rst
     _ -> putStrLn "Usage: main \"<json_string>\""
-
-processJSON :: BL.ByteString -> IO ()
-processJSON input =
-  case parseAutomergeSpans input of
-    Left err -> putStrLn $ "Error: " ++ err
-    Right spans -> print spans
