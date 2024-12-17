0dca3d9be46cc97b3c472a53446927250d496463
Aris Goudouras <aris.gk@gmail.com>
2024-12-17 14:52:50 +0200

    feat: add format options

--- app/Cli.hs
+++ app/Cli.hs
@@ -1,16 +1,30 @@
-module Cli (Command (..), readInputCommand) where
+module Cli (Command (..), readInputCommand, Format (..)) where
 
-import Options.Applicative (Parser, argument, command, execParser, fullDesc, helper, info, metavar, progDesc, str, subparser, (<**>))
+import Options.Applicative (Parser, ReadM, argument, command, eitherReader, execParser, fullDesc, help, helper, info, long, metavar, option, progDesc, str, subparser, (<**>))
 
 data Command
-  = ConvertFromAutomerge String
+  = ConvertFromAutomerge Format String
   | ConvertToAutomerge String
   deriving (Show)
+
+data Format = Pandoc | Automerge | Markdown | HTML deriving (Show)
+
+formatParser :: Parser Format
+formatParser = option readFormat (long "to" <> metavar "FORMAT" <> help "Specify the format (pandoc, automerge, markdown, html)")
+
+readFormat :: ReadM Format
+readFormat = eitherReader $ \arg ->
+  case arg of
+    "pandoc" -> Right Pandoc
+    "automerge" -> Right Automerge
+    "markdown" -> Right Markdown
+    "html" -> Right HTML
+    _ -> Left $ "Unknown format: " ++ arg
 
 commandParser :: Parser Command
 commandParser =
   subparser
-    ( command "fromAutomerge" (info (ConvertFromAutomerge <$> argument str (metavar "AUTOMERGE_SPANS_JSON")) (progDesc "Convert from Automerge Spans JSON to Markdown"))
+    ( command "fromAutomerge" (info (ConvertFromAutomerge <$> formatParser <*> argument str (metavar "AUTOMERGE_SPANS_JSON")) (progDesc "Convert from Automerge Spans JSON to the output format"))
         <> command "toAutomerge" (info (ConvertToAutomerge <$> argument str (metavar "MARKDOWN_DATA")) (progDesc "Convert from Markdown to Automerge Spans JSON"))
     )
 
--- app/Main.hs
+++ app/Main.hs
@@ -1,26 +1,33 @@
 module Main (main) where
 
 import Automerge (parseAutomergeSpans)
-import Cli (Command (..), readInputCommand)
+import Cli (Command (..), Format (..), readInputCommand)
 import qualified Data.ByteString.Lazy.Char8 as BL
 import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
 import PandocReader (toPandoc)
 import PandocWriter (writeAutomergeSpans)
-import Text.Pandoc (def, readMarkdown)
+import Text.Pandoc (Pandoc, PandocMonad, WriterOptions, def, readMarkdown)
 import Text.Pandoc.Class (runIO)
 import Text.Pandoc.Error (handleError)
-import Text.Pandoc.Writers (writeMarkdown)
+import Text.Pandoc.Writers (writeHtml5String, writeMarkdown, writeNative)
 
-convertFromAutomerge :: String -> IO ()
-convertFromAutomerge input = do
+writeTo :: (PandocMonad m) => Format -> WriterOptions -> Pandoc -> m T.Text
+writeTo format = case format of
+  Cli.Pandoc -> writeNative
+  Cli.Automerge -> writeAutomergeSpans
+  Cli.Markdown -> writeMarkdown
+  Cli.HTML -> writeHtml5String
+
+convertFromAutomerge :: Format -> String -> IO ()
+convertFromAutomerge format input = do
   let automergeSpans = parseAutomergeSpans $ BL.pack input
   case automergeSpans of
     Left err -> putStrLn $ "Error: " ++ err
     Right spans -> do
       result <- runIO $ do
         doc <- toPandoc spans
-        writeMarkdown def doc
+        writeTo format def doc
       rst <- handleError result
       TIO.putStrLn rst
 
@@ -36,5 +43,5 @@ main :: IO ()
 main = do
   command <- readInputCommand
   case command of
-    ConvertFromAutomerge jsonString -> convertFromAutomerge jsonString
+    ConvertFromAutomerge format jsonString -> convertFromAutomerge format jsonString
     ConvertToAutomerge markdownString -> convertToAutomerge markdownString
