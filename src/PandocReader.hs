5bf10a69e29bb9fd85a968c6f4b1296d49c55973
Aris Goudouras <aris.gk@gmail.com>
2025-02-11 22:42:12 +0200

    chore: remove old functions

--- src/PandocReader.hs
+++ src/PandocReader.hs
@@ -4,15 +4,14 @@ import Automerge (AutomergeSpan (..), BlockMarker (..), BlockSpan (..), Heading 
 import Control.Monad.Except (throwError)
 import Data.List (find, groupBy)
 import Data.Maybe (fromMaybe)
-import Data.Sequence as Seq (Seq (Empty))
 import qualified Data.Text as T
 import Data.Tree (Tree (Node), drawTree, foldTree, unfoldForest)
 import Debug.Trace
 import Text.Pandoc (PandocError (PandocSyntaxMapError))
-import Text.Pandoc.Builder (Blocks, Inlines, Many (..), blockQuote, codeBlockWith, doc, emph, fromList, headerWith, link, para, str, strong, toList)
+import Text.Pandoc.Builder (Blocks, Inlines, doc, emph, fromList, link, str, strong, toList)
 import Text.Pandoc.Class
 import Text.Pandoc.Definition
-import Utils.Sequence (firstValue, lastValue, withoutLast)
+import Utils.Sequence (firstValue)
 
 data BlockNode = PandocBlock Block | BulletListItem | OrderedListItem deriving (Show)
 
@@ -155,39 +154,6 @@ treeNodeToPandocBlock node childrenNodes = case node of
 
     firstInline :: Inlines -> Maybe Inline
     firstInline = firstValue
-
-toPandoc'' :: (PandocMonad m) => [AutomergeSpan] -> m Pandoc
-toPandoc'' spans = pure . doc $ convertAutomergeSpans spans
-
-convertAutomergeSpans :: [AutomergeSpan] -> Blocks
-convertAutomergeSpans = foldl' convertAutomergeSpan (Many Seq.Empty)
-
-convertAutomergeSpan :: Blocks -> AutomergeSpan -> Blocks
-convertAutomergeSpan acc (BlockSpan (AutomergeBlock blockMarker _)) = acc <> (fromList [convertBlockMarker blockMarker])
-convertAutomergeSpan acc (TextSpan textSpan) = case lastValue acc of
-  Nothing -> acc <> convertAndWrapToParagraph textSpan
-  Just block -> withoutLast acc <> convertAndAddTo block textSpan
-
-convertBlockMarker :: BlockMarker -> Block
-convertBlockMarker blockSpan = case blockSpan of
-  ParagraphMarker -> Para []
-  HeadingMarker (Heading (HeadingLevel level)) -> Header level nullAttr []
-  CodeBlockMarker -> CodeBlock nullAttr T.empty
-  _ -> undefined -- more blocks to be implemented
-
-convertAndWrapToParagraph :: TextSpan -> Blocks
-convertAndWrapToParagraph = para . convertTextSpan
-
-convertAndAddTo :: Block -> TextSpan -> Blocks
-convertAndAddTo block textSpan = case block of
-  Para inlines -> para $ addTextSpanToInlines (fromList inlines) textSpan
-  Header level attr inlines -> headerWith attr level $ addTextSpanToInlines (fromList inlines) textSpan
-  CodeBlock attr _ -> codeBlockWith attr $ value textSpan
-  BlockQuote _ -> blockQuote $ convertAndWrapToParagraph textSpan
-  _ -> undefined
-
-addTextSpanToInlines :: Inlines -> TextSpan -> Inlines
-addTextSpanToInlines inlines textSpan = inlines <> convertTextSpan textSpan
 
 convertTextSpan :: TextSpan -> Inlines
 convertTextSpan = convertMarksToInlines <*> convertTextToInlines
